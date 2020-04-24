0c674ddafe9ed64181df03f29426620038a205d1
omarsamir <omarsamsoliman@gmail.com>
2020-04-24 12:30:26 +0200

    feah: add logout API

--- src/routers/user.js
+++ src/routers/user.js
@@ -33,6 +33,30 @@ router.post('/users/login', async (req,res) => {
     }
 })
 
+router.post('/users/logout',auth,async (req,res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter ((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+
+router.post('/users/logoutAll',auth,async (req,res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+
 // auth is middleware
 router.get('/users/me',auth,(req,res) => {
     res.send(req.user)
